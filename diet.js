42d0bc4d7b5ec3a21f58432439419b44b4d226fb
Matheus Caetano Rocha <matheuscaetanorocha@gmail.com>
2023-10-17 18:34:28 -0300

    Start implement meal, food and food dropdown on js

--- diet.js
+++ diet.js
@@ -1,8 +1,4 @@
-import Sortable from 'sortablejs';
-
-
-var el = document.getElementById('items');
-var sortable = Sortable.create(el);
+/*import Sortable from 'sortablejs';*/  
 
 class Food {
     constructor(name, calories, proteins, carbs, fats) {
@@ -31,26 +27,99 @@ const milkSemiSkimmed = new Food("Semi-Skimmed Milk"); // 100 g
 const milkSkimmed = new Food("Skimmed Milk"); // 100 g
 const milkSoy = new Food("Soy Milk", 54, 3.27, 6.28, 1.75); // 100 g
 
-function main() {
-    do {
-        console.log("Choose an option:\n");
-        console.log("1. Add meal");
-        console.log("2. Edit meal");
-        console.log("3. ");
+const foodItems = [
+    egg, banana, rice, beans, chickenGrilled, porkRibs, porkLoin, porkLinguica,
+    guava, oat, peanutButter, raisins, milkWhole, milkSemiSkimmed, milkSkimmed, milkSoy
+];
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const mealsContainer = document.getElementById("meals-container");
+    /*new Sortable(mealsContainer, {
+        group: "meals",
+        animation: 150,
+    });*/
+
+    document.getElementById("create-meal").addEventListener("click", () => {
+        const mealList = document.createElement("ul");
+        mealList.className = "sortable"; // Make it sortable
+        /*mealList.textContent = "Meal";*/
+        mealsContainer.appendChild(mealList);
+
+        const mealTitle = document.createElement("input");
+        mealTitle.className = "form-control-dark";
+        mealTitle.setAttribute('id', 'floatingInput');
+        mealTitle.setAttribute('type', 'text');
+        mealTitle.setAttribute('placeholder', 'Meal Name');
+        mealList.appendChild(mealTitle);
         
-        const choice = prompt("Enter your choice: ");
-        switch (choice) {
-            case "1":
-                // Add meal logic
-                break;
-            case "2":
-                // Edit meal logic
-                break;
-            case "3":
-                // Your other cases
-                break;
-            default:
-                console.log("Invalid choice");
-        }
-    } while (choice)
-}
+        const createFood = document.createElement("button");
+        createFood.className = "btn btn-dark";
+        createFood.setAttribute('id', 'create-food');
+        createFood.textContent = 'Create Food';
+        mealList.appendChild(createFood);
+
+        document.getElementById("create-food").addEventListener("click", () => {
+            const foodItem = document.createElement("li");
+            mealList.insertBefore(foodItem, mealList.lastChild);
+
+            const foodDropdown = document.createElement("div");
+            foodDropdown.className = "dropdown";
+            foodItem.appendChild(foodDropdown);
+
+            const foodDropdownButton = document.createElement("button");
+            foodDropdownButton.className = "btn btn-secondary dropdown-toggle";
+            foodDropdownButton.setAttribute('id', 'dropdownMenuButton1');
+            foodDropdownButton.setAttribute('data-bs-toggle', 'dropdown');
+            foodDropdownButton.setAttribute('aria-expanded', 'false');
+            foodDropdownButton.textContent = 'Food Options';
+            foodDropdown.appendChild(foodDropdownButton);
+
+            const foodDropdownMenu = document.createElement("div");
+            foodDropdownMenu.className = "dropdown-menu dropdown-menu-dark";
+            foodDropdownMenu.setAttribute('aria-labelledby', 'dropdownMenuButton1');
+            foodDropdownButton.appendChild(foodDropdownMenu);
+
+            const newFoodOption = document.createElement("a");
+            newFoodOption.className = "dropdown-item";
+            newFoodOption.textContent = "New food";
+            foodDropdownMenu.appendChild(newFoodOption);
+
+            const foodOptionSeparator = document.createElement("hr");
+            foodOptionSeparator.className = "dropdown-divider";
+            foodDropdownMenu.appendChild(foodOptionSeparator);
+        
+            foodItems.forEach(Food => {
+                const foodOption = document.createElement("a");
+                foodOption.className = "dropdown-item";
+                foodOption.textContent = Food.name; // Use the name of the food
+                // Attach an event listener for when the user selects this food
+                foodOption.addEventListener("click", () => {
+                    // Add logic here to handle the selected food item
+                    console.log(`Selected: ${Food.name}`);
+                });
+                foodDropdownMenu.appendChild(foodOption);
+            });
+        });
+
+
+
+    });
+
+        /*new Sortable(mealList, {
+            group: "meals", // Ensure the meal is in the same group as others
+            animation: 150,
+        });*/
+
+    /* Add food items to the meal
+    function addFoodToMeal(mealList, foodName, foodCalories, foodProteins, foodCarbs, foodFats) {
+        const foodItem = document.createElement("li");
+        foodItem.textContent = foodName;
+        // Add more elements, including dropdowns, inputs, etc.
+        mealList.appendChild(foodItem);
+    }
+
+    var el = document.getElementById('items');
+    var sortable = Sortable.create(el);*/
+
+});
