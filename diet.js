6cab168676fbe70c8e05226d0a882ed02fa3c225
Matheus Caetano Rocha <matheuscaetanorocha@gmail.com>
2023-10-18 03:46:20 -0300

    Finish implementation of most frontend features

--- diet.js
+++ diet.js
@@ -59,6 +59,40 @@ document.addEventListener("DOMContentLoaded", () => {
         createFood.textContent = 'Create Food';
         mealList.appendChild(createFood);
 
+        const mealTotal = document.createElement("div");
+        mealTotal.className = "container-fluid";
+        mealList.appendChild(mealTotal);
+
+        const totalTitle = document.createElement("h3");
+        totalTitle.textContent = "Total:"
+        mealTotal.appendChild(totalTitle);
+
+        const totalRow = document.createElement("div");
+        totalRow.className = "row";
+        mealTotal.appendChild(totalRow)
+
+        const totalColumns = [
+            { id: "calories", label: "Calories:", class: "col-12 col-sm-6 col-md-3" },
+            { id: "proteins", label: "Proteins:", class: "col-12 col-sm-6 col-md-3" },
+            { id: "carbs", label: "Carbs:", class: "col-12 col-sm-6 col-md-3" },
+            { id: "fats", label: "Fats:", class: "col-12 col-sm-6 col-md-3" },
+        ];
+
+        totalColumns.forEach((column) => {
+            const totalColDiv = document.createElement("div");
+            totalColDiv.className = column.class;
+            const totalLabel = document.createElement("label");
+            totalLabel.htmlFor = column.id;
+            totalLabel.textContent = column.label;
+            const input = document.createElement("input");
+            input.type = "text";
+            input.id = column.id;
+            input.disabled = true;
+            totalColDiv.appendChild(totalLabel);
+            totalColDiv.appendChild(input);
+            totalRow.appendChild(totalColDiv);
+        });
+
         document.getElementById("create-food").addEventListener("click", () => {
             const foodItem = document.createElement("li");
             mealList.insertBefore(foodItem, mealList.lastChild);
@@ -69,6 +103,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
             const foodDropdownButton = document.createElement("button");
             foodDropdownButton.className = "btn btn-secondary dropdown-toggle";
+            foodDropdownButton.style.width = "100%";
             foodDropdownButton.setAttribute('id', 'dropdownMenuButton1');
             foodDropdownButton.setAttribute('data-bs-toggle', 'dropdown');
             foodDropdownButton.setAttribute('aria-expanded', 'false');
@@ -76,7 +111,7 @@ document.addEventListener("DOMContentLoaded", () => {
             foodDropdown.appendChild(foodDropdownButton);
 
             const foodDropdownMenu = document.createElement("div");
-            foodDropdownMenu.className = "dropdown-menu dropdown-menu-dark";
+            foodDropdownMenu.className = "dropdown-menu dropdown-menu-dark row";
             foodDropdownMenu.setAttribute('aria-labelledby', 'dropdownMenuButton1');
             foodDropdownButton.appendChild(foodDropdownMenu);
 
@@ -88,22 +123,31 @@ document.addEventListener("DOMContentLoaded", () => {
             const foodOptionSeparator = document.createElement("hr");
             foodOptionSeparator.className = "dropdown-divider";
             foodDropdownMenu.appendChild(foodOptionSeparator);
-        
+
+            const foodOptionsContainer = document.createElement("div");
+            foodOptionsContainer.className = "container-fluid";
+            foodDropdownMenu.appendChild(foodOptionsContainer);
+
+            const foodOptionsRow = document.createElement("div");
+            foodOptionsRow.className = "row";
+            foodOptionsContainer.appendChild(foodOptionsRow);
+
             foodItems.forEach(Food => {
+
+                const foodOptionsColumn = document.createElement("div");
+                foodOptionsColumn.className = "col-6 col-sm-4 col-md-3 col-lg-2"
+                foodOptionsRow.appendChild(foodOptionsColumn);
+
                 const foodOption = document.createElement("a");
                 foodOption.className = "dropdown-item";
-                foodOption.textContent = Food.name; // Use the name of the food
-                // Attach an event listener for when the user selects this food
+                foodOption.textContent = Food.name;
                 foodOption.addEventListener("click", () => {
                     // Add logic here to handle the selected food item
                     console.log(`Selected: ${Food.name}`);
                 });
-                foodDropdownMenu.appendChild(foodOption);
+                foodOptionsColumn.appendChild(foodOption);
             });
         });
-
-
-
     });
 
         /*new Sortable(mealList, {
@@ -111,15 +155,7 @@ document.addEventListener("DOMContentLoaded", () => {
             animation: 150,
         });*/
 
-    /* Add food items to the meal
-    function addFoodToMeal(mealList, foodName, foodCalories, foodProteins, foodCarbs, foodFats) {
-        const foodItem = document.createElement("li");
-        foodItem.textContent = foodName;
-        // Add more elements, including dropdowns, inputs, etc.
-        mealList.appendChild(foodItem);
-    }
-
-    var el = document.getElementById('items');
+    /*var el = document.getElementById('items');
     var sortable = Sortable.create(el);*/
 
 });
