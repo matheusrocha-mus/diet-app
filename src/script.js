487fef6285aa72dbbc16ef73e0f489f2fedecec0
Matheus Caetano Rocha <matheuscaetanorocha@gmail.com>
2023-11-01 16:41:54 -0300

    Reorganize js code into functions

--- src/script.js
+++ src/script.js
@@ -1,22 +1,3 @@
-/*import Sortable from 'sortablejs';*/  
-
-window.addEventListener('load', () => {
-    const savedData = localStorage.getItem('yourKey'); // Replace 'yourKey' with a unique key.
-    if (savedData) {
-        // Parse the JSON data and use it to set the page state.
-        const parsedData = JSON.parse(savedData);
-        // Update your page elements with the data.
-    }
-    else {
-        console.error('Data not found.');
-    }
-});
-
-function saveDataToLocalStorage(data) {
-    const jsonData = JSON.stringify(data);
-    localStorage.setItem('yourKey', jsonData); // Replace 'yourKey' with a unique key.
-}
-
 let foodItems = [];
 
  // Requeijão: 260, 8, 3.33, 24
@@ -25,202 +6,211 @@ fetch('foodData.json')
     .then((response) => response.json())
     .then((data) => {
         foodItems = data;
-
-        const foodOptionsRow = document.getElementById("food-options-row");
-        foodItems.forEach(Food => {
-            const foodOptionsColumn = document.createElement("div");
-            foodOptionsColumn.className = "col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2"
-            foodOptionsRow.appendChild(foodOptionsColumn);
-            const foodOption = document.createElement("a");
-            foodOption.className = "dropdown-item";
-            foodOption.textContent = Food.name;
-            foodOption.addEventListener("click", () => {
-                console.log(`Selected: ${Food.name}`);
-            });
-            foodOptionsColumn.appendChild(foodOption);
-        });
-
     })
     .catch((error) => console.error('Error loading food data:', error));
 
+(function () {
+    'use strict'
+    var forms = document.querySelectorAll('form')
+    Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+        form.classList.add('was-validated')
+        }, false)
+    })
+})()
 
 
-var totalCalories = 0.0;
-var totalProteins = 0.0;
-var totalCarbs = 0.0;
-var totalFats = 0.0;
 
-var mealTotalCalories = 0.0;
-var mealTotalProteins = 0.0;
-var mealTotalCarbs = 0.0;
-var mealTotalFats = 0.0;
+function createMeal (mealsList, uniqueIdCounter) {
+    const uniqueId = `${uniqueIdCounter}`;
+    const meal = document.createElement("li");
+    meal.className = "meal sortable list-group-item";
+    meal.innerHTML = `
+    <input type="text" placeholder="Enter meal name" class="meal-title form-control-dark fs-3">
+    <ul class="foods-list list-group">
+        <!--Generated foods will be appended here-->
+    </ul>
+    <button class="add-food btn btn-dark">Add food</button>
+    <div class="meal-total container-fluid">
+        <h3>Total:</h3>
+        <div class="row">
+            <div class="col-12 col-sm-6 col-md-3">
+                <label for="meal-calories${uniqueId}">Calories:</label>
+                <input class="meal-calories" type="number" id="meal-calories${uniqueId}" disabled>
+            </div>
+            <div class="col-12 col-sm-6 col-md-3">
+                <label for="meal-proteins${uniqueId}">Proteins:</label>
+                <input class="meal-proteins" type="number" id="meal-proteins${uniqueId}" disabled>
+            </div>
+            <div class="col-12 col-sm-6 col-md-3">
+                <label for="meal-carbs${uniqueId}">Carbs:</label>
+                <input class="meal-carbs" type="number" id="meal-carbs${uniqueId}" disabled>
+            </div>
+            <div class="col-12 col-sm-6 col-md-3">
+                <label for="meal-fats${uniqueId}">Fats:</label>
+                <input class="meal-fats" type="number" id="meal-fats${uniqueId}" disabled>
+            </div>
+        </div>
+    </div>
+    `;
+    mealsList.appendChild(meal);
+}
+
+function addFood (foodsList, uniqueIdCounter) {
+    const uniqueId = `${uniqueIdCounter}`;
+    const food = document.createElement("li");
+    food.className = "food container-fluid list-group-item list-group-item-dark p-4 rounded"; // Avaliar esse nome bosta (duas instâncias abaixo)
+    food.innerHTML = `
+    <div class="food-item-row row">
+        <div class="col-12 col-md-6 col-lg-8 col-xl-10">
+            <div class="food-options-dropdown dropdown col-12 col-sm-10">
+                <button class="btn btn-secondary dropdown-toggle" id="food-dropdown-button${uniqueId}" data-bs-toggle="dropdown" aria-expanded="false">Food Options
+                    <div class="dropdown-menu dropdown-menu-dark row" aria-labelledby="food-dropdown-button${uniqueId}">
+                        <div class="container-fluid">
+                            <div class="food-options-row row">
+                                <!--Get options from foodData.json-->
+                            </div>
+                        </div>
+                    </div>
+                </button>
+            </div>
+        </div>
+        <div class="col-6 col-md-3 col-lg-2 col-xl-1">
+            <button class="create-new-food btn btn-primary" type="button">Create food</button>
+        </div>
+        <div class="col-6 col-md-3 col-lg-2 col-xl-1">
+            <button class="delete-food btn btn-danger" type="button">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"></path>
+                    <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"></path>
+                </svg> Delete
+            </button>
+        </div>
+    </div>
+    `;
+    foodsList.appendChild(food);
+
+    const foodOptionsRow = food.querySelector(".food-options-row");
+    foodItems.forEach(Food => {
+        const foodOptionsColumn = document.createElement("div");
+        foodOptionsColumn.className = "col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2"
+        foodOptionsRow.appendChild(foodOptionsColumn);
+        const foodOption = document.createElement("a");
+        foodOption.className = "dropdown-item";
+        foodOption.textContent = Food.name;
+        foodOption.addEventListener("click", () => {
+            console.log(`Selected: ${Food.name}`);
+        });
+        foodOptionsColumn.appendChild(foodOption);
+    });
+
+}
+
+function createFood (food, uniqueIdCounter) {
+    const uniqueId = `${uniqueIdCounter}`;
+    food.querySelector(".food-item-row").remove(); // Avaliar esse nome bosta
+    const newFood = document.createElement("form");
+    newFood.className = "new-food container-fluid needs-validation";
+    newFood.setAttribute("novalidate", "");
+    newFood.innerHTML = `
+    <div class="row">
+        <div class="new-food-form col-12 col-md-10">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12 col-md-4 d-flex">
+                        <label for="new-food-name${uniqueId}">Name: </label>
+                        <input class="form-control new-food-name" required type="text" id="new-food-name${uniqueId}" placeholder="Food name">
+                    </div>
+                    <div class="col-12 col-md-4 d-flex>
+                        <label for="new-food-calories${uniqueId}">Calories: </label>
+                        <input class="form-control new-food-calories" required type="number" id="new-food-calories${uniqueId}" placeholder="Calories (in grams, only numbers)">
+                    </div>
+                    <div class="col-12 col-md-4 d-flex>
+                        <label for="new-food-proteins${uniqueId}">Proteins: </label>
+                        <input class="form-control new-food-proteins" required type="number" id="new-food-proteins${uniqueId}" placeholder="Proteins (in grams, only numbers)">
+                    </div>
+                    <div class="col-12 col-md-4 d-flex>
+                        <label for="new-food-carbs${uniqueId}">Carbs: </label>
+                        <input class="form-control new-food-carbs" required type="number" id="new-food-carbs${uniqueId}" placeholder="Carbs (in grams, only numbers)">
+                    </div>
+                    <div class="col-12 col-md-4 d-flex>
+                        <label for="new-food-fats${uniqueId}">Fats: </label>
+                        <input class="form-control new-food-fats" required type="number" id="new-food-fats${uniqueId}" placeholder="Fats (in grams, only numbers)">
+                    </div>
+                    <div class="col-12 col-md-4 d-flex>
+                        <label for="new-food-portion${uniqueId}">Portion: </label>
+                        <select class="form-select new-food-portion" required id="new-food-portion${uniqueId}">
+                            <option selected disabled>Select portion type</option>
+                            <option value="1 unit">1 unit</option>
+                            <option value="100 g">100 g</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="new-food-buttons" class="col-12 col-md-2">
+            <div class="container-fluid">
+                <div class="row">
+                    <button class="cancel-new-food col-5 col-md-12 btn btn-danger" type="button">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"></path>
+                            <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"></path>
+                        </svg> Cancel
+                    </button>
+                    <button class="confirm-new-food col-5 col-md-12 btn btn-success" type="submit">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
+                        <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/>
+                        </svg> Create
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+    `;
+    food.appendChild(newFood);
+}
 
 
 
 document.addEventListener("DOMContentLoaded", () => {
 
-    const mealsContainer = document.getElementById("meals-container");
+    const mealsList = document.getElementById("meals-list");
+    let uniqueIdCounter = 0;
 
     document.getElementById("create-meal").addEventListener("click", () => {
-        const meal = document.createElement("ul");
-        meal.className = "meal sortable list-group";
-        mealsContainer.appendChild(meal);
+        createMeal(mealsList, uniqueIdCounter);
+        uniqueIdCounter++;
+    });
 
+    document.addEventListener("click", (event) => {
+        if (event.target.classList.contains("add-food")) {
+            const foodsList = event.target.previousElementSibling;
+            addFood(foodsList, uniqueIdCounter);
+            uniqueIdCounter++;
+        }
 
+        else if (event.target.classList.contains("create-new-food")) {
+            const food = event.target.closest(".food");
+            createFood (food, uniqueIdCounter)
+            uniqueIdCounter++;
+        }
 
-        const mealTitle = document.createElement("input");
-        mealTitle.className = "form-control-dark";
-        mealTitle.className = "meal-title";
-        mealTitle.setAttribute('type', 'text');
-        mealTitle.setAttribute('placeholder', 'Enter the meal name');
-        meal.appendChild(mealTitle);
-
-        // Create a new class to represent a meal name.
-        class MealTitle {
-            constructor(title) {
-            this.name = title;
+        else if (event.target.classList.contains("delete-food")) {
+            const food = event.target.closest(".food");
+            if (food) {
+                food.remove();
             }
         }
 
-        // Add a listener to the meal name inputs.
-        const mealTitleInputs = document.querySelectorAll(".meal-title");
-            for (const mealTitleInput of mealTitleInputs) {
-            mealTitleInput.addEventListener("blur", () => {
-                // Get the meal name from the input.
-                const mealTitle = mealTitleInput.value;
-
-                // Create a new instance of the meal name class.
-                const mealTitleInstance = new MealTitle(mealTitle);
-
-                // Add the meal name instance to an array.
-                const mealTitles = [];
-                mealTitles.push(mealTitleInstance);
-
-                // Serialize the array to JSON and save it to the JSON file.
-                const jsonString = JSON.stringify(mealTitles);
-                localStorage.setItem("mealTitles", jsonString);
-            });
-        }
-
-        // Get the meal names from the JSON file.
-        const mealTitlesJsonString = localStorage.getItem("mealTitles");
-
-        // If the meal names JSON string is not empty, parse it to an object.
-        if (mealTitlesJsonString) {
-            const mealTitles = JSON.parse(mealTitlesJsonString);
-
-            // Display the meal names in the meal box.
-            const meal = document.querySelector(".meal");
-            for (const mealTitle of mealTitles) {
-                const mealItem = document.createElement("li");
-                mealItem.textContent = mealTitle;
-                meal.appendChild(mealItem);
-            }
-        }
-
-
-
-        const createFood = document.createElement("button");
-        createFood.className = "create-food btn btn-dark";
-        createFood.textContent = 'Create Food';
-        meal.appendChild(createFood);
-
-        const createFoodButtons = document.querySelectorAll(".create-food");
-        createFoodButtons.forEach(button => {
-            button.addEventListener("click", () => {
-                const foodItem = document.createElement("li");
-                foodItem.className = "food-item sortable container-fluid list-group-item list-group-item-dark p-4 rounded";
-                meal.insertBefore(foodItem, meal.lastChild);
-
-                foodItem.appendChild(document.getElementById("food-item-row"));
-                document.getElementById("food-item-row").classList.remove("d-none");
-
-                document.getElementById("create-new-food").addEventListener("click", () => {
-                    foodItem.appendChild(document.getElementById("new-food"));
-                    document.getElementById("new-food").classList.remove("d-none");
-                    document.getElementById("food-item-row").classList.add("d-none");
-
-                    const cancelButton = document.getElementById("cancel-form-button");
-                    cancelButton.addEventListener("click", () => {
-                        document.getElementById("new-food").style.display = "none";
-                        document.getElementById("food-item-row").style.display = "none";
-                        const foodItem = cancelButton.closest(".food-item");
-                        mealsContainer.appendChild(document.getElementById("new-food"));
-                        mealsContainer.appendChild(document.getElementById("food-item-row"));
-                        foodItem.parentNode.removeChild(foodItem);
-                    });
-
-                    const createFoodButton = document.getElementById("success-form-button");
-
-                    createFoodButton.addEventListener("click", () => {
-                        const foodName = document.getElementById("new-food-name").value;
-                        const foodCalories = document.getElementById("new-food-calories").value;
-                        const foodProteins = document.getElementById("new-food-proteins").value;
-                        const foodCarbs = document.getElementById("new-food-carbs").value;
-                        const foodFats = document.getElementById("new-food-fats").value;
-
-                        const newFood = {
-                            name: foodName,
-                            calories: foodCalories,
-                            proteins: foodProteins,
-                            carbs: foodCarbs,
-                            fats: foodFats,
-                        };
-
-                        const foodDataJsonString = localStorage.getItem("foodData");
-                        let foodData = [];
-
-                        if (foodDataJsonString) {
-                            foodData = JSON.parse(foodDataJsonString);
-                        }
-
-                        foodData.push(newFood);
-
-                        const newFoodDataJsonString = JSON.stringify(foodData);
-                        localStorage.setItem("foodData", newFoodDataJsonString);
-
-                        // Hide the form and show the food item row.
-                        document.getElementById("new-food").classList.add("d-none");
-                        document.getElementById("food-item-row").classList.remove("d-none");
-                    });
-                });
-            });
-        });
-
-
-
-        const mealTotal = document.createElement("div");
-        mealTotal.className = "container-fluid";
-        meal.appendChild(mealTotal);
-
-        const totalTitle = document.createElement("h3");
-        totalTitle.textContent = "Total:"
-        mealTotal.appendChild(totalTitle);
-
-        const totalRow = document.createElement("div");
-        totalRow.className = "row";
-        mealTotal.appendChild(totalRow)
-
-        const totalColumns = [
-            { id: "calories", label: "Calories: ", class: "col-12 col-sm-6 col-md-3" },
-            { id: "proteins", label: "Proteins: ", class: "col-12 col-sm-6 col-md-3" },
-            { id: "carbs", label: "Carbs: ", class: "col-12 col-sm-6 col-md-3" },
-            { id: "fats", label: "Fats: ", class: "col-12 col-sm-6 col-md-3" },
-        ];
-
-        totalColumns.forEach((column) => {
-            const totalColDiv = document.createElement("div");
-            totalColDiv.className = column.class;
-            const totalLabel = document.createElement("label");
-            totalLabel.htmlFor = column.id;
-            totalLabel.textContent = column.label;
-            const totalInput = document.createElement("input");
-            totalInput.type = "text";
-            totalInput.id = column.id;
-            totalInput.disabled = true;
-            totalColDiv.appendChild(totalLabel);
-            totalColDiv.appendChild(totalInput);
-            totalRow.appendChild(totalColDiv);
-        });
+        /*else if (event.target.classList.contains("confirm-new-food")) { // Successfull form submit
+            const food = event.target.closest(".food");
+            food.querySelector(".new-food").remove();
+            food.appendChild(foodItem);
+        }*/
     });
 });
